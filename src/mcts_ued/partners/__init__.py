532c2f2ac7cfcd4dda73d1175492795a49efc50f
Mouhssine Rifaki <235592255+m-rifaki@users.noreply.github.com>
2026-04-14 11:15:00 -0400

    partners subpackage

---
+++ src/mcts_ued/partners/__init__.py
@@ -0,0 +1,9 @@
+"""Partner-policy uncertainty set."""
+from mcts_ued.partners.parametrize import (
+    BoxConfig,
+    BoxPartnerSpace,
+    PartnerSpace,
+    PartnerSpec,
+)
+
+__all__ = ["BoxConfig", "BoxPartnerSpace", "PartnerSpace", "PartnerSpec"]
